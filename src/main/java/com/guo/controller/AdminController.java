df41aba4c373c90422f246f53d0235c9b1cff923
gokufriday <1561413067@qq.com>
2020-08-06 21:38:56 +0800

    =commit somes

--- src/main/java/com/guo/controller/AdminController.java
+++ src/main/java/com/guo/controller/AdminController.java
@@ -7,6 +7,8 @@ import com.guo.config.RabbitMqConfig;
 import com.guo.entity.Post;
 import com.guo.search.mq.entity.PostMqIndexMessage;
 import com.guo.vo.PostVO;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PostMapping;
